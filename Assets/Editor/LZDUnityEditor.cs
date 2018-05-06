423ecc1a6df1eeaf83a78da3f381e6fd843969d5
DESKTOP-L0G7LHG\15735 <1106169797@qq.com>
2018-05-06 10:11:06 +0800

    更新打包工具

---
+++ Assets/Editor/LZDUnityEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+/*
+*AUTHOR: #AUTHOR#
+*CREATETIME: #CREATETIME#
+*DESCRIPTION: 
+*/
+namespace lzdUnityEditor
+{
+    public class LZDUnityEditor
+    {
+        private const string root_name = "LZDUnityEditor";
+        [MenuItem(root_name + "/BundleObject",false,100)]
+        private static void createBundle()
+        {
+            MyScriptObject.CreateScriptObject<BundleObject>("Bundle");
+        }
+
+    }
+}
