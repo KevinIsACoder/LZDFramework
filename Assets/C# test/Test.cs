d7599f7d6a6a0ad05da867cec0e8a0f697c9c78b
zhendong.liang <zhendong.liang@gaea.com>
2019-06-18 12:06:11 +0800

    add project

---
+++ Assets/C# test/Test.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// AUTHOR: 梁振东
+// DATETIME: 06/17/2019 18:53:32
+// DESC: ****
+public class Test : MonoBehaviour {
+	// private Dictionary<string, string> dic = new Dictionary<string>;
+
+	// Use this for initialization
+	void Start () {
+		IDictionary<string, string> Idic = new Dictionary<string, string>();
+		Dictionary<string, string> dic = new Dictionary<string, string>();
+		Idic.Add("lzd","1234");
+		dic.Add("lzd", "lzd");
+		TestDic(Idic);
+		TestDic(dic);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void TestDic(IDictionary<string, string> testIdic)
+	{
+        testIdic["lzd"] = "5678";
+	}
+}
